6e9f90b054ca8a4eefb268f7f10419123faa7e66
Morwal <sumitmor@Sumits-MacBook-Air.local>
2026-05-16 22:44:02 +0530

    add vercel url in cors

--- backend/main.py
+++ backend/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://localhost:5174",  # Default Vite port
         "http://localhost:3000",  # Alternative common port
         "http://127.0.0.1:5174",
+        "https://tech-news-project.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
